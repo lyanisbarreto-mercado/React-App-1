54f5bea5d16030f5b0d0a8ac82bcc662f219e761
Lyanis Rubí Barreto Mercado <lyanis.barreto@gmail.com>
2026-02-13 16:52:30 -0500

    Updated Sidebar.jsx to complete TODO tasks

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,37 +4,47 @@ import { useState, useCallback } from "react"
  * unordered list.
  * @returns Component
  */
-export default function Sidebar() {
-  let [newMenuItem, setNewMenuItem] = useState("")
-  // TODO: 2 Using a state hook, maintain the current menu items as an array state.
-  // let [menuItems, setMenuItems] = useState(initialMenuItems)
-  let [filter, setFilter] = useState("")
-  // Adds a single string passed in as parameter to the state element
-  // "menuItems" that holds the set of current menu items.
-  let addMenuItem = useCallback(() => {
+export default function Sidebar({initialMenuItems = []}) { //uses initialMenuItems as a prop, or if nothing is passed, an empty array
+  let [newMenuItem, setNewMenuItem] = useState("") //lets the current "new menu item" be blank, so that it can be inputted
+
+  let [menuItems, setMenuItems] = useState(initialMenuItems) //sets the current set of menuitems as the original list 
+
+  let [filter, setFilter] = useState("") //lets the filter be blank to be inputted
+  // Adds a single string passed in as a parameter to the state element "menuItems" that holds the set of current menu items.
+
+  let addMenuItem = useCallback((item) => { //adds an individual item as a parameter, this is what we insert within the addMenuItem input
+    if (!item.trim()) return  //if the item is blank or there is nothing, then reset to previous state
+    setMenuItems([...menuItems, newMenuItem]) //takes the menuItems, and adds the newMenuItem
     console.log("Added menu item")
-    //   // TODO: 3. Add a new menu item to the correct variable associated with this class.
-    //   // This involves adding a parameter and changing a class instance variable (props).
-    //   setMenuItems([item, ...menuItems])
-  }, [])
+    }, [menuItems, newMenuItem]) //dependency array, if any one of those values changes, it rerenders
+  
+    
+  let regex = new RegExp(filter, "i") //"i" means "case-insensitive" without having to make everything lowercase. so using the "filter" input, it searches it and makes it disregard the difference between 
+  const filteredName = menuItems.filter((item) => 
+     regex.test(item)) //for each item, it each one using the regex, and tests it against the filter input (which is part of the regex)
 
-  // TODO: 4. Display ONLY the menu items that contain the filter element value
-  // "term" in them. Each menu item should be an unordered list item wrapped in an unordered list (ul) element.
-
-  // TODO: 1 Render inside the outer div an unordered list of the menu items, with each string in the array
-  // its own item.
   return (
     <div>
+      <ul>
+        {filteredName.map((item, index) => ( //takes the filtered array (since it'll update based on the filter), and for each item...
+          <li key={index}>{item}</li> //it renders each item!
+        ))}
+      </ul>
+    
       <input
         type="text"
         id="newMenuItemValue"
         value={newMenuItem}
         onChange={(event) => setNewMenuItem(event.target.value)}
+        //event - takes the change 
+        //target - looks to the property the change happened to (which is the menuItem list)
+        //the value of the change
+        //This happens at every change of the value as it is inputted. More obvious in filtering, as it updates the list that is rendered
       ></input>
       <br />
       <button
         onClick={() => {
-          /* TODO: 3 */
+          addMenuItem(newMenuItem) //it adds the newMenuItem input into the array and renders it to the list
         }}
       >
         Add Item
@@ -45,7 +55,7 @@ export default function Sidebar() {
         type="text"
         value={filter}
         onChange={(event) => setFilter(event.target.value)}
-        placeholder="Filter by..."
+        placeholder="Filter by..." 
       ></input>
     </div>
   )
